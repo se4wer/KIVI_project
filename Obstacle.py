40c4c5bf757b75f2854b59de6fec6f1295e14522
Egor Prokofjev <trueananas17@gmail.com>
2024-03-19 19:00:22 +0300

    added fish

--- Obstacle.py
+++ Obstacle.py
@@ -5,7 +5,7 @@ from kivy.uix.widget import Widget
 
 
 class Obstacle(Widget):
-    def __init__(self, pos = None, object_id = 1, texture_path = 'stone texture.png', **kwargs):
+    def __init__(self, pos = None, object_id = 1, texture_path = 'fish.png', **kwargs):
         super().__init__(**kwargs)
         if pos == None:
             pos = 0,0
