d221dc8e9813630e1a8fa54ae283dbfa9507c12a
ARSENIY <akirikowski@gmail.com>
2024-03-19 01:31:51 +0100

    Collision working!

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
+    NumericProperty, ReferenceListProperty, ObjectProperty, ListProperty
 )
 from kivy.vector import Vector
 from kivy.clock import Clock
@@ -19,15 +19,21 @@ Free_fall_acceleration = 9.81
 
 
 class Obstacle(Widget):
+    def __init__(self, pos = None, object_id = 1, **kwargs):
+        super().__init__(**kwargs)
+        if pos == None:
+            pos = 0,0
+        self.id = object_id
+        self.pos = pos
+        self.size = 10, 100
     def obstacle_collision(self, ball):
         if self.collide_widget(ball):
-            print("collision")
+            print(f"collision with {self.id}")
+            return True
+
 
 
 class PongBall(Widget):
-    pos_x = NumericProperty(0)
-    pos_y = NumericProperty(0)
-    pos = ReferenceListProperty(pos_x, pos_y)
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
@@ -44,35 +50,38 @@ class PongGame(Widget):
     start = False
     after = False
     obstacles_added = False
-    obstacle1 = ObjectProperty(None)
+    obstacle: Obstacle() = ObjectProperty(None)
+    obstacles: [Obstacle()] = ListProperty([])
 
     def startGame(self):
         self.start = True
 
-    def addObstacles(self):
-        print("obstacles added")
-        # self.obstacle1 = Widget()
-        # self.add_widget(self.obstacle1)
-        # with self.obstacle1.canvas:
-        #     Rectangle(pos=(600, 600), size=(100, 100))
-        self.obstacles_added = True
+    def remove_obstacle(self, obstacle):
+        self.remove_widget(obstacle)
+        self.obstacles.remove(obstacle)
 
     def serve_ball(self, ang, coef):
         self.ball_released = True
-        self.ball.pos_x = SCREEN_WIDTH / 3
-        self.ball.pos_y = SCREEN_HEIGHT / 3
-        Initial_vertical_velocity = Initial_velocity * sin(ang) * coef
-        Initial_horizontal_velocity = Initial_velocity * cos(ang) * coef
-        self.ball.velocity = (Initial_horizontal_velocity, Initial_vertical_velocity)
-        self.ball.center = self.center
+        self.ball.pos = SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3
+        self.ball.velocity = (Initial_velocity * cos(ang) * coef, Initial_velocity * sin(ang) * coef)
 
     def spawn_ball(self):
-        self.ball.pos_x = SCREEN_WIDTH / 3
-        self.ball.pos_y = SCREEN_HEIGHT / 3
+        self.ball.pos = SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3
         self.ball.velocity = (0, 0)
         self.ball_released = False
+    def addObstacles(self, pos, object_id, n_of_obstacles):
+        print("obstacles added")
+        for i in range(n_of_obstacles):
+            obstacle = Obstacle(pos = (500 + 40 * i, 400), object_id = i)
+            self.add_widget(obstacle)
+            self.obstacles.append(obstacle)
+        self.obstacles_added = True
 
     def update(self, dt):
+        if self.obstacles_added:
+            for obstacle in self.obstacles:
+                if obstacle.obstacle_collision(self.ball):
+                    self.remove_obstacle(obstacle)
         if self.ball_released:
             self.ball.move()
         if self.start:
@@ -81,12 +90,7 @@ class PongGame(Widget):
             self.remove_widget(but)
             self.start = False
             self.after = True
-        if self.obstacles_added:
-            self.obstacle1.obstacle_collision(self.ball)
 
-    # def on_touch_down(self, touch):
-    #     if start == True:
-    #         self.spawn_ball()
     def on_touch_down(self, touch):
         if self.after:
             self.spawn_ball()
@@ -103,7 +107,7 @@ class PongGame(Widget):
 
 
 class PongApp(App):
-    def build(self):
+    def build(self) -> Widget():
         Window.size = (SCREEN_WIDTH, SCREEN_HEIGHT)
         game = PongGame()
         Clock.schedule_interval(game.update, 1.0 / Frame_rate)
